abb0d09742e4d5c533b6f8d4f4094a281985fc63
joedysonbezerra <joedysonbezerra@gmail.com>
2018-09-22 23:09:03 -0300

    Criando estrutura de lista encadeada

---
+++ Lista/C/linkedList.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+struct product {
+   char name[10];
+   int quantity;
+};
+
+typedef struct { 
+   struct product products;
+   struct no *next;
+}no;
+
+int main(int argc, char **argv) {
+
+  return 0;
+
+}
