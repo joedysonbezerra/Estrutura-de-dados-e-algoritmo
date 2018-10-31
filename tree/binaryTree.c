8395e377d36f4527b52bed40fb7aec5e96b3edfe
joedysonbezerra <joedysonbezerra@gmail.com>
2018-10-31 13:36:23 -0300

    Organização do codigo e implementação de isEmpty, freeTree.

--- tree/binaryTree.c
+++ tree/binaryTree.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define False 0;
-#define True 1;
+#define FALSE 0;
+#define TRUE 1;
 
-typedef struct Node{
+typedef struct Node {
    int value;
    struct Node *left;
    struct Node *right;
@@ -11,6 +11,8 @@ typedef struct Node{
 
 BinaryTree *insert();
 BinaryTree *create();
+BinaryTree *freeTree();
+int isEmpty();
 int search();
 void print();
 void erro();
@@ -20,10 +22,12 @@ int main(int argc, char **argv) {
    tree = insert(5,insert(6,create(),insert(8,create(),create())),insert(7,create(),create()));
    print(tree);
    printf("%d\n",search(tree,7));
+   tree = freeTree(tree);
+   print(tree);
 
 
 }
-BinaryTree *create(){
+BinaryTree *create() {
    return NULL;
 }
 
@@ -39,28 +43,38 @@ BinaryTree *insert(int pValue, struct Node *left, struct Node *right) {
    return new;
 }
 
-void print(BinaryTree * base){
-	if(base != NULL){
+void print(BinaryTree *base) {
+	if(!isEmpty(base)) {
 		printf("%d\n", base->value);
 		print(base->left);
 		print(base->right);
    }
 }
-int search(BinaryTree * base, int value){
-   if(base == NULL){
-      return False;
-   }else{
-      if(base->value == value){
-         return True;
-      }else{
+int search(BinaryTree *base, int value) {
+   if(isEmpty(base)) {
+      return FALSE;
+   }else {
+      if(base->value == value) {
+         return TRUE;
+      }else {
          search(base->left,value);
          search(base->right,value);
       }
    }
 }
+int isEmpty(BinaryTree *base) {
+   return(base == NULL);
+}
 
+BinaryTree *freeTree(BinaryTree *base) {
+   if(!isEmpty(base)) {
+      freeTree(base->left);
+      freeTree(base->right);
+      free(base);
+   }
+   return NULL;
 
-
+}
 void erro(char message[]) {
    printf("%s\n",message);
 }
