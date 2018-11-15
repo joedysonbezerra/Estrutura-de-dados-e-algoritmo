729b7e39d8d5ee2c70bf85016360a18cb8a362bd
joedysonbezerra <joedysonbezerra@gmail.com>
2018-11-14 22:50:19 -0300

    Inicio do desafio de EDA + sistema de arquivo

---
+++ EDA_Faculdade_exercicios/q4.c
@@ -0,0 +1,12 @@
+// Num sistema de arquivos, um catálogo de todos os arquivos é organizado como 
+// uma árvore de busca binária. Cada nó denota um arquivo e especifica seu nome e, 
+// entre outras coisas,a data de seu último acesso, codificada como um inteiro. 
+// Escreva um programa que percorra a árvore e apague todos os arquivos cujos 
+// últimos acessos tenham sido anteriores a uma certa data. 
+// As chaves do catálogo são os nomes dos arquivos.
+
+#include <stdio.h>
+
+int main(int argc, char **argv){
+
+}
